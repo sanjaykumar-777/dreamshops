9a477ce53d5c0d7ac7b6493dbca32065477ef935
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-09-10 16:00:35 +0530

    initial commit

---
+++ src/test/java/com/example/sanjaykumar_777/dreamshops/DreamshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanjaykumar_777.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
