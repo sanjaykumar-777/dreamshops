d06f950eacfab25530ad460842f0a2c381b66d55
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-09-12 00:23:31 +0530

    added lombok annotations to Category class

--- src/main/java/com/example/sanjaykumar_777/dreamshops/model/Category.java
+++ src/main/java/com/example/sanjaykumar_777/dreamshops/model/Category.java
@@ -1,10 +1,21 @@
 package com.example.sanjaykumar_777.dreamshops.model;
 
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
