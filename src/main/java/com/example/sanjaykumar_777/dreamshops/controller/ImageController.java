c2e7d97d14f51d7c5431ea3f6853ae09cd51c058
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-09-21 21:37:04 +0530

    delete image controller

--- src/main/java/com/example/sanjaykumar_777/dreamshops/controller/ImageController.java
+++ src/main/java/com/example/sanjaykumar_777/dreamshops/controller/ImageController.java
@@ -1,19 +1,23 @@
 package com.example.sanjaykumar_777.dreamshops.controller;
 
 import com.example.sanjaykumar_777.dreamshops.dto.ImageDto;
+import com.example.sanjaykumar_777.dreamshops.exception.ResourceNotFoundException;
+import com.example.sanjaykumar_777.dreamshops.model.Image;
 import com.example.sanjaykumar_777.dreamshops.response.ApiResponse;
 import com.example.sanjaykumar_777.dreamshops.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 @RestController
@@ -33,4 +37,19 @@ public class ImageController {
         }
 
     }
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if(image != null) {
+                imageService.deleteImageById( imageId);
+                return ResponseEntity.ok(new ApiResponse("Delete success!", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Delete failed!", INTERNAL_SERVER_ERROR));
+    }
+
 }
