ac72a94506a2f0fe01050668b606b0fed39e96c1
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-09-23 23:19:46 +0530

    no major changes

--- src/main/java/com/example/sanjaykumar_777/dreamshops/controller/ProductController.java
+++ src/main/java/com/example/sanjaykumar_777/dreamshops/controller/ProductController.java
@@ -1,4 +1,20 @@
 package com.example.sanjaykumar_777.dreamshops.controller;
 
+import com.example.sanjaykumar_777.dreamshops.model.Product;
+import com.example.sanjaykumar_777.dreamshops.response.ApiResponse;
+import com.example.sanjaykumar_777.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
 }
